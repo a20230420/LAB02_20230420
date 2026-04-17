e3df77cc0724c9d9701d7ac20b12d24b674ff907
Moreno <moreno@example.com>
2026-04-16 19:15:01 -0500

    Primera subida

---
+++ src/main/java/com/example/lab2_20230420/Lab220230420Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20230420;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220230420Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220230420Application.class, args);
+    }
+
+}
